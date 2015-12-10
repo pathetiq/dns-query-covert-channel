1f3e11d48a46e1c1c4608330fa699950f6c2f5d6
Interference Security <int3rf3r3nc3@gmail.com>
2015-12-11 00:09:15 +0530

    Added argparse for port

--- dns_server.py
+++ dns_server.py
@@ -5,13 +5,19 @@ Created on 2015-11-27
 '''
 
 import socket
+import argparse
+
+parser = argparse.ArgumentParser(description='Process args.')
+parser.add_argument('-p','--port', help='DNS server port')
+args = parser.parse_args()
+dns_port = args.port.encode('utf-8')
 
 socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 # DNS listener on 53
-listen_on = ("",53)
+listen_on = ("",int(dns_port))
 socket.bind(listen_on)
 
 while True:
-	data,source = socket.recvfrom(1024)
-        print data.strip()
+    data,source = socket.recvfrom(1024)
+    print data.strip()
